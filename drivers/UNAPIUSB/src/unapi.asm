c8f789ebd6cab2db3354e4787214a762d9021188
Mario Smit (S0urceror) <mario.smit.nl@gmail.com>
2020-02-22 18:58:17 +0100

    Updated GPL T&C's

--- drivers/UNAPIUSB/src/unapi.asm
+++ drivers/UNAPIUSB/src/unapi.asm
@@ -1,3 +1,19 @@
+;
+; unapi.ASM - UNAPI compliant MSX USB driver
+; Copyright (c) 2020 Mario Smit (S0urceror)
+; 
+; This program is free software: you can redistribute it and/or modify  
+; it under the terms of the GNU General Public License as published by  
+; the Free Software Foundation, version 3.
+;
+; This program is distributed in the hope that it will be useful, but 
+; WITHOUT ANY WARRANTY; without even the implied warranty of 
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+; General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License 
+; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;
 ARG:    equ  0F847h
 HOKVLD: equ  0FB20h
 EXPTBL: equ  0FCC1h
--- drivers/UnapiKbd/src/tsr.asm
+++ drivers/UnapiKbd/src/tsr.asm
@@ -1,3 +1,22 @@
+;
+; tsr.ASM - The resident bit of the USB HID keyboard driver.
+;           Will be put in a free memory mapper segment.
+;
+; Copyright (c) 2020 Mario Smit (S0urceror)
+; 
+; This program is free software: you can redistribute it and/or modify  
+; it under the terms of the GNU General Public License as published by  
+; the Free Software Foundation, version 3.
+;
+; This program is distributed in the hope that it will be useful, but 
+; WITHOUT ANY WARRANTY; without even the implied warranty of 
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+; General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License 
+; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;
+
 CSRSW: equ 0FCA9h
 PUTPNT EQU 0F3F8h
 GETPNT EQU 0F3FAh
