7c87ebfd13141d90c4675bd90dfdd3efd782ac22
Mario Smit (S0urceror) <mario.smit.nl@gmail.com>
2020-06-16 09:27:45 +0200

    Discrete PCB discontinued, flash changed AM29F040

--- software/src/flash.c
+++ software/src/flash.c
@@ -192,7 +192,10 @@ BOOL flash_ident ()
     uint8_t manufacturer = flash_segment[0];
     uint8_t device = flash_segment[1];
     myprint ("M: %x, D: %x\r\n",manufacturer,device);
-    if (device==0xa4)  // device ID is correct
+    // Am29F040B = A4
+    // M29F800AB = 58
+    // SST39SF040 = B7
+    if (device==0xa4 || device==0x58 || device==0xB7)  // device ID is correct
     {
         // reset
         flash_segment[0] = 0xf0;
